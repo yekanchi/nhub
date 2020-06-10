9613434eb19766af42355c991a90d72f095d6ffc
yekanchi <email@address.com>
2020-06-10 18:26:24 +0430

    added some new fixes

---
+++ bin/consoles/pwsh_Integrated_Console.ps1
@@ -0,0 +1,3 @@
+E:\NHUB\bin\consoles\set_commons_defaults
+clear;
+Write-Color -Text 'CustomShell:',' pwsh_Integrated_Console.bat' -Color Green, Blue;
---
+++ bin/consoles/set_commons_defaults.ps1
@@ -0,0 +1,1 @@
+$env:path += ";E:\NHUB\bin\vsCode;E:\NHUB\bin\gitscm\bin;E:\NHUB\bin\compilers\node;E:\NHUB\bin\compilers\node\NPMCache";
