bb69af8918b6cf409703ea06a4949f4bd1b80552
yekanchi <email@address.com>
2020-08-01 21:37:36 +0430

    configured powershell scripts

--- bin/consoles/set_commons.ps1
+++ bin/consoles/set_commons.ps1
@@ -1,6 +1,7 @@
-$env:clearlog = "1"
+$env:clearlog = "0"
+E:\NHUB\bin\consoles\set_modules
 E:\NHUB\bin\consoles\set_envs
 E:\NHUB\bin\consoles\set_aliases
 
-
+#Set Paradox Theme for oh-my-posh
 Set-Theme Paradox;
